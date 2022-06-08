2ab962343fadd371aac0892e7b0bc7defa565990
Karemu1842call <victorkinoti03@gmail.com>
2022-06-08 15:16:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(thing="roller-skate"){
+    return `This Saturday, I want to ${thing}!`
+}
+function mondayWork(todo ="go to the office"){
+    return `This Monday, I will ${todo}.`
+}
+
+function wrapAdjective(style="*"){
+    function inner (special ="a hard worker"){
+        return `You are ${style}${special}${style}!`
+    }
+    return inner
+}
